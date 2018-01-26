30dd94275118daf0c9fd961bed062a0be4d75776
Unknown <tim.torzewski@charter.net>
2018-01-25 20:25:43 -0600

    Tried to make throttle work
    and added a bit more debuging stuff

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -16,8 +16,10 @@ void DriveTrain::InitDefaultCommand() {
 void DriveTrain::Drive(frc::Joystick*stick) {
 	double stickY = stick->GetY();
 	double stickZ = stick->GetZ();
+	double stickT = stick->GetThrottle();
 	SmartDashboard::PutNumber("Y", stickY);
 	SmartDashboard::PutNumber("Z", stickZ);
+	SmartDashboard::PutNumber("T", stickT);
 	double stickY2 = DriveFunctionY(stickY);
 	double stickZ2 = DriveFunctionZ(stickZ);
 	SmartDashboard::PutNumber("Y2", stickY2);
@@ -31,11 +33,13 @@ void DriveTrain::Stop() {
 
 double DriveTrain::DriveFunctionY(double inSpeed) {
 	double outSpeed = .25*pow(inSpeed, 3) + .75*inSpeed;
+	SmartDashboard::PutNumber("FY", outSpeed);
 	return outSpeed;
 }
 
 double DriveTrain::DriveFunctionZ(double inSpeed) {
 	double outSpeed = .8*pow(inSpeed, 3) + .2*inSpeed;
+	SmartDashboard::PutNumber("FZ", outSpeed);
 	return outSpeed;
 }
 
